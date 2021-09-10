c8a780d8beee7ce19faeb39019af901695ae40db
Anwar <AnwarAldogmi@gmail.com>
2021-09-09 17:25:32 -0700

    wip

---
+++ data/seeds/03-ingredients.js
@@ -0,0 +1,12 @@
+const ingredients = [
+    { ingredient_name: 'Broccoli', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pesto', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Pasta', ingredient_unit: 'lbs' },
+    { ingredient_name: 'Lemon', ingredient_unit: 'slices' },
+    { ingredient_name: 'Chicken', ingredient_unit: 'kilos' },
+    { ingredient_name: 'Salmon', ingredient_unit: 'grams' }
+]
+
+exports.seed = function (knex) {
+    return knex('ingredients').insert(ingredients)
+}
